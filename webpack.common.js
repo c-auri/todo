da76034892fd2634f1f54130f317acdbd50239b3
c-auri <43008483+c-auri@users.noreply.github.com>
2023-03-14 17:17:35 +0100

    Install SASS

--- webpack.common.js
+++ webpack.common.js
@@ -1,33 +1,55 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    devtool: 'source-map',
-    optimization: {
-        usedExports: true
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        // disable type checker - we will use it in fork plugin
-                        transpileOnly: true
-                    }
-                }
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
+  optimization: {
+    usedExports: true
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            // disable type checker - we will use it in fork plugin
+            transpileOnly: true
+          }
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+            sourceMap: true
             }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    plugins: []
+          }
+        ]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].css'
+    }),
+   ]
 };
