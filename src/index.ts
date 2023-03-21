52129a3bd4b6867fda6afdd1f1e6744ac453e2f3
c-auri <43008483+c-auri@users.noreply.github.com>
2023-03-21 20:01:00 +0100

    Add todo class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { Todo } from './components/Todo'
 import './styles.scss'
+
+const todoList = document.querySelector('#todo-list')
+
+const todos = [
+    new Todo('Add styles'),
+    new Todo('Add add button'),
+    new Todo('Add delete button'),
+]
+
+todos.forEach(todo => todoList.appendChild(todo.render()))
