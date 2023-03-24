0e07d828278f54b8c428aa86fa1ca4dcecc1cb5a
c-auri <43008483+c-auri@users.noreply.github.com>
2023-03-24 20:02:27 +0100

    Move renderTodo to TodoList

--- src/components/TodoList.ts
+++ src/components/TodoList.ts
@@ -2,7 +2,6 @@ import './TodoList.scss'
 
 import { Project } from '../models/Project'
 import { Todo } from '../models/Todo'
-import { renderTodo } from "./Todo"
 
 const todoListId = 'todo-list'
 
@@ -22,6 +21,25 @@ export function renderTodoList(project : Project) {
 
 export function appendTodoList(todo : Todo) {
     document.getElementById(todoListId).appendChild(renderTodo(todo, deleteTodo))
+}
+
+function renderTodo(todo : Todo, deleteTodo : (todo: Element) => void) {
+    const li = document.createElement('li')
+    li.classList.add('todo')
+    li.setAttribute('data-id', ""+todo.id)
+
+    const title = document.createElement('span')
+    title.classList.add('todo__title')
+    title.innerHTML = todo.title
+
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('todo__button')
+    deleteButton.innerHTML = "x"
+    deleteButton.addEventListener('click', () => deleteTodo(li))
+
+    li.appendChild(title)
+    li.appendChild(deleteButton)
+    return li
 }
 
 function deleteTodo(li : Element) {
