ec2ac9e7430cbfd1c0892e3bd0ef4f9abc35c303
c-auri <43008483+c-auri@users.noreply.github.com>
2023-04-02 18:44:48 +0200

    Refactor createTodoInput

--- src/js/views/TodoInput.ts
+++ src/js/views/TodoInput.ts
@@ -3,9 +3,27 @@ import { createTodo } from '../models/Todo'
 
 export function createTodoInput() {
     const div = document.createElement('div')
+
+    const dialog = createDialog()
+    const newButton = createNewButton(dialog)
+
+    div.appendChild(dialog)
+    div.appendChild(newButton)
+
+    return div
+}
+
+function createDialog() {
     const dialog = document.createElement('dialog')
 
-    dialog.innerHTML = `
+    dialog.innerHTML = getDialogHtml()
+    dialog.addEventListener('close', () => submitTodo(dialog))
+
+    return dialog
+}
+
+function getDialogHtml() {
+    return `
         <form method='dialog' class='new-todo'>
             <div class='new-todo__inputs'>
                 <label class='new-todo__label'>
@@ -19,30 +37,30 @@ export function createTodoInput() {
             </div>
         </form>
     `
+}
 
-    dialog.addEventListener('close', () => {
-        const titleInput = dialog.querySelector('#new-todo__title') as HTMLInputElement
+function submitTodo(dialog: HTMLDialogElement) {
+    const titleInput = dialog.querySelector('#new-todo__title') as HTMLInputElement
 
-        if (dialog.returnValue === 'add') {
-            const title = titleInput.value
-    
-            if (title) {
-                appendTodo(createTodo(title))
-            }
+    if (dialog.returnValue === 'add') {
+        const title = titleInput.value
+
+        if (title) {
+            appendTodo(createTodo(title))
         }
+    }
 
-        titleInput.value = ''
-    })
+    titleInput.value = ''
+}
 
+function createNewButton(dialog: HTMLDialogElement) {
     const newButton = document.createElement('button')
+
     newButton.id = 'new-todo__btn'
     newButton.classList.add('button')
     newButton.classList.add('button--new')
     newButton.innerHTML = 'Add New Todo'
     newButton.addEventListener('click', () => dialog.showModal())
 
-    div.appendChild(dialog)
-    div.appendChild(newButton)
-
-    return div
+    return newButton
 }
