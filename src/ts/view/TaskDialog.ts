5455f6b259035e0ed1d1131564ae1ee827475100
c-auri <43008483+c-auri@users.noreply.github.com>
2023-04-11 20:29:38 +0200

    Extract getTask

--- src/ts/view/TaskDialog.ts
+++ src/ts/view/TaskDialog.ts
@@ -1,3 +1,4 @@
+import { Task } from "../model/Task"
 import { appendTask, getCurrentProject } from "../Controller"
 import { v4 as uuid } from 'uuid'
 import { set } from 'date-fns'
@@ -36,26 +37,34 @@ function resetDialog() {
 }
 
 function submitTask(dialog: HTMLDialogElement): void {
+    const task = getTask()
+
+    if (!task) {
+        return
+    }
+
+    if (dialog.returnValue === 'add') {
+        appendTask(task)
+    }
+}
+
+function getTask(): Task | undefined {
+    const dialog = document.querySelector('#new-task-dialog') as HTMLDialogElement
     const titleInput = dialog.querySelector('#new-task-title') as HTMLInputElement
     const descriptionInput = dialog.querySelector('#new-task-description') as HTMLTextAreaElement
     const dateInput = dialog.querySelector('#new-task-date') as HTMLInputElement
     const timeInput = dialog.querySelector('#new-task-time') as HTMLInputElement
+    
+    const { date, hasTime } = getDueDateInfo(dateInput.value, timeInput.value)
 
-    if (dialog.returnValue === 'add') {
-        const title = titleInput.value
-        const { date, hasTime } = getDueDateInfo(dateInput.value, timeInput.value)
-        const project = getCurrentProject()
-
-        if (title) {
-            appendTask({
-                id:uuid(),
-                title,
-                project,
-                description: descriptionInput.value,
-                date: date,
-                hasTime: hasTime })
-        }
-    }
+    return titleInput.value ? {
+        id: uuid(),
+        title: titleInput.value,
+        project: getCurrentProject(),
+        description: descriptionInput.value,
+        date,
+        hasTime
+    } : undefined
 }
 
 function getDueDateInfo(dateString: string, time: string): { date: Date, hasTime: boolean } {
